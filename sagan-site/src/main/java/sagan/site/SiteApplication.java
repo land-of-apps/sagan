b9800c1f97e9421fea4a47e9a436182e6c73677f
Kevin Gilpin <kgilpin@gmail.com>
2021-08-05 15:48:15 -0400

    feat: Add 'checkpoint' helper method to SiteApplication

--- sagan-site/src/main/java/sagan/site/SiteApplication.java
+++ sagan-site/src/main/java/sagan/site/SiteApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * The entry point for the Sagan web application.
  */
@@ -15,4 +17,19 @@ public class SiteApplication {
         SpringApplication.run(SiteApplication.class, args);
     }
 
+    public static String checkpoint(String filePath) {
+        try {
+            Object recorder = Class.forName("com.appland.appmap.record.Recorder").getMethod("getInstance").invoke(null);
+            Boolean hasActiveSession = (Boolean) recorder.getClass().getMethod("hasActiveSession").invoke(recorder);
+            if ( !hasActiveSession.booleanValue() ) {
+                return "There's no AppMap recording in progress. Start an AppMap recording in order to take a snapshot.";
+            }
+
+            Object recording = recorder.getClass().getMethod("checkpoint").invoke(recorder);
+            recording.getClass().getMethod("moveTo", String.class).invoke(recording, filePath);
+            return filePath;
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
